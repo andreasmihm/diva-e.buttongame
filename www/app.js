f5f63d94ac51ea99f699cfe50a9bb01c9a47f0e0
Andreas Mihm <andreas.mihm@ageto.net>
2015-05-20 13:17:28 +0200

    spiel für lina

--- www/app.js
+++ www/app.js
@@ -1,26 +1,68 @@
+var time = Date.now();
+var round = 0;
+var bestclick = 1000;
+
 var page = tabris.create("Page", {
-  title: "Hello, World!",
+  title: "Spiel für Lina",
   topLevel: true
 });
 
- 
-var textView = tabris.create("TextView", {
-  font: "24px",
-  layoutData: {centerX: 0, top: 100},
-  text: "Hello World 123"
+
+var text = tabris.create("TextView", {
+  layoutData: {left: 10, top: 20, right: 10},
+  text: "Hallo Lina, sobald der Button erscheint, dürcke ihn so schnell Du kannst. Das Spiel sagt Dir wie schnell Du warst und merkst sich Deine Bestzeit. Viel Spaß!",
+  alignment: "left"
 }).appendTo(page);
 
-// Create a push button and add it to the page
-var button = tabris.create("Button", {
-  text: "Ein Button 50 pix unter textview",
-  layoutData: {centerX: 0, top: [textView, 50]}
+
+var text = tabris.create("TextView", {
+  layoutData: {left: 10, top: 80, right: 10},
+  text: "Bestzeit:",
+  alignment: "left"
 }).appendTo(page);
- 
-// Change the text view's text when the button is pressed
-button.on("selection", function() {
-  textView.set("text", "Totally Rock!");
-});
+
+var text2 = tabris.create("TextView", {
+  layoutData: {left: 10, top: 140, right: 10},
+  text: "Aktuelle Reaktionszeit:",
+  alignment: "left"
+}).appendTo(page);
+
+/*
+tabris.create("Button", {
+  layoutData: {left: 10, top: 10},
+  text: "Button"
+}).on("select", function() {
+  this.set("text", "Pressed " + (++round) + " times");
+}).appendTo(page);
+*/
+
+tabris.create("Button", {
+  layoutData: {centerX: 0, centerY: 0},
+  text: "Press me!"
+}).on("select", function() {
+  var clicktime = Date.now() - time;
+  text2.set("text", "Aktuelle Reaktionszeit: " + clicktime);
+  if (clicktime < bestclick) {
+	bestclick = clicktime;
+	text.set("text", "Bestzeit: " + bestclick);
+  }
+
+  this.set("text", "Bitte warte...");
+  this.set("visible", false);
+  setTimeout(function() {
+    time = Date.now();
+  this.set("layoutData", {centerX: getRandomInt(-100, 100), centerY: getRandomInt(-200, 200)});
+	this.set("text", "GoGoGo!");
+  this.set("visible", true);
+  }.bind(this), 2000);
+}).appendTo(page);
 
 
- 
- page.open();
+// Returns a random integer between min (included) and max (excluded)
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+page.open();
